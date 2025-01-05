c3845bff72a25b5a0bd36cb27db5936b6adb129d
Marina Ebbecke <m.ebbecke@gmail.com>
2025-01-05 17:57:20 -0300

    Aula 2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { CreateGoal } from "./components/create-goal"
+import { Summary } from "./components/summary"
+import { Dialog } from "./components/ui/dialog"
+
+export function App() {
+  return (
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
+  )
+}
